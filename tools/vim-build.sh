7624fb3f5e03dc3cff53cceb9d34a524c82b201b
lsq <lsqypj.gmail.com>
2025-03-03 13:53:20 +0800

    add pacman repo support

--- tools/vim-build.sh
+++ tools/vim-build.sh
@@ -160,3 +160,11 @@ else
     echo "$releaseLog" >"$basedir"/../gitlog.txt
 fi
 cat "$basedir"/../gitlog.txt
+
+zstFiles=(*.pkg.tar.zst)
+if [ -e "${files[0]}" ]; then
+    repo-add "lsq.db.tar.zst" *.pkg.tar.zst
+fi
+
+7z a -mx=9 vim-build-src.7z src PKGBUILD libXmp if_ver.vim
+ls ./
